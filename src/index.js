0235123bcd92855755681592a2e49f7f5e3b666e
Kyle Groening <kyle.groening@cytiva.com>
2023-01-02 17:35:25 -0500

    Add import statements and basic fn calls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import {TodoItem, Projects} from './todoObjects'
+import TodoItem, {Projects} from './todoObjects'
+import {Page} from './DOMContoller'
+import './style.css'
+
+Projects.addProject('Test Project');
+Projects.setCurrentProject('Test Project')
+Projects.addTodo(TodoItem('Test item','test desc','today','1'))
+Projects.addTodo(TodoItem('Another Test item','test desc','today','1'))
+// console.log(Projects.projectList[Projects.currentProject][0].title)
+
+Page.setupSidebar();
+Page.setupMain();
+Page.refreshTodoList(Projects.projectList, Projects.currentProject);
+Page.refreshProjectList(Projects.projectList)
 
 
